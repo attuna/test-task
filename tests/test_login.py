b269c2bef301806bfe82928068b8cb1d89b6902f
Hanna Peshko <93200268+attuna@users.noreply.github.com>
2023-07-05 20:42:25 +0200

    Feature/lanuage support (#1)
     modify results in accordance with language

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 from parameterized import parameterized
@@ -5,14 +6,12 @@ from parameterized import parameterized
 from utils.base import BaseTestCase
 from pages.login import LoginPage
 
-base_url = "https://www.limango.pl/"
-
 
 class NegativeLoginTestCase(BaseTestCase):
     @parameterized.expand([
-        ("invalid_username@gmail.com", "invalid_password", base_url),
-        ("invalid_username@gmail.com", "valid_password", base_url),
-        ("valid_username@gmail.com", "another_invalid_password", base_url),
+        ("invalid_username@gmail.com", "invalid_password", os.getenv('url')),
+        ("invalid_username@gmail.com", "valid_password", os.getenv('url')),
+        ("valid_username@gmail.com", "another_invalid_password", os.getenv('url')),
     ])
     def test_negative_login(self, username, password, url):
         login_page = LoginPage(self.driver)
@@ -21,6 +20,8 @@ class NegativeLoginTestCase(BaseTestCase):
         login_page.enter_username(username)
         login_page.enter_password(password)
         login_page.submit_login_form()
+        messages = login_page.load_messages()
+        self.assertEqual(login_page.is_password_error_message_displayed(), messages['invalid_login_error_message'])
 
 
 if __name__ == '__main__':
